e19e7868dc76dc29fe624c19bdf064b07453f1aa
Joe McGrath <josephmcg01@gmail.com>
2023-01-30 18:07:20 +0900

    test(constants): improve constants doc and add config coverage

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,6 @@
-// update these as you like, but ensure NUM_TOTAL_STORIES is divisible by NUM_PER_PAGE
+// update these as you like, but ensure:
+// - NUM_TOTAL_STORIES is divisible by NUM_PER_PAGE
+// - NUM_TOTAL_STORIES is less than or equal to 500
+// https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty
 export const NUM_TOTAL_STORIES = 100;
 export const NUM_PER_PAGE = 25;
--- test/constants.test.ts
+++ test/constants.test.ts
@@ -4,4 +4,7 @@ describe("constants", () => {
   test("NUM_TOTAL_STORIES is divisible by NUM_PER_PAGE", () => {
     expect(NUM_TOTAL_STORIES % NUM_PER_PAGE).toEqual(0);
   });
+  test("NUM_TOTAL_STORIES is less than or equal to 500", () => {
+    expect(NUM_TOTAL_STORIES <= 500).toBeTruthy();
+  });
 });
